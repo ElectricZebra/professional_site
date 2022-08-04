c81c8928644b056af426ddb06b3656c6cb49d045
ElectricZebra <nicholasregoli@gmail.com>
2022-08-03 19:30:39 -0700

    test for remote

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // init commit
+  // test commit for remote
+
   return (
     <div className="App">
       <header className="App-header">
